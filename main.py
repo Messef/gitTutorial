01caa999824812b4139f540821b104c2ba6e1c2a
Akku Bear <85040583+Messef@users.noreply.github.com>
2021-11-05 16:10:27 +0000

    I made a function that makes it so that main.py can say "Hello World" and then loop through a tuple while making one random statement. However, I want to make an if statement that continues to the next iteration if z==nums[i].

---
+++ main.py
@@ -0,0 +1,15 @@
+print("Hello World!")
+import random
+def myNums():
+  i=0
+  y=random.randrange(4)
+  z=nums[y]
+  print("I like " + z)
+  for x in nums:
+    print("I like "+ str(nums[i]) + " too!")
+    i = i+1
+nums = ["1", "2", "3", "4"]
+myNums()
+
+
+
