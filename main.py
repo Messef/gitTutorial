c9eedf48daa8f0862d79c86986ad04fd1176d52d
Akku Bear <85040583+Messef@users.noreply.github.com>
2021-11-08 22:56:17 +0000

    I have moved on! We are moving on to the classes instead of the random number and looping it. Cheers to a new future!

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
-print("Hello World!")
-import random
-def myNums():
-  i=0
-  y=random.randrange(4)
-  z=nums[y]
-  print("I like " + z)
-  for x in nums:
-    print("I like "+ str(nums[i]) + " too!")
-    i = i+1
-nums = ["1", "2", "3", "4"]
-myNums()
+class Animal:
+  def __init__(bird,species, name, age, action):
+    bird.species=species
+    bird.name=name
+    bird.age=age
+    bird.action=action
 
-
-
+  def itsAction(bird):
+   print("A "+bird.species+" named as " + bird.name+ " who is " + bird.age + " just " + bird.action)
+bird1=Animal("Cat", "Johnny","5","be happy")
+bird1.itsAction()
